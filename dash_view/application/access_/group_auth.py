f424bb0809f43f8b834162646346dd3f885e764b
luoja <luoja@dfmc.com.cn>
2024-11-06 15:10:01 +0800

    添加团队管理员的授权页面

--- dash_view/application/access_/group_auth.py
+++ dash_view/application/access_/group_auth.py
@@ -24,3 +24,31 @@ icon = None
 order = 4
 access_metas = ('团队授权-页面',)
 logger = Log.get_logger(__name__)
+
+
+def render_content(menu_access: MenuAccess, **kwargs):
+    return Card(
+        Table(
+            id='group-mgmt-table',
+            columns=[
+                {'title': _('团队名称'), 'dataIndex': 'group_name', 'width': '10%'},
+                {'title': _('团队描述'), 'dataIndex': 'group_remark', 'width': '15%'},
+                {'title': _('用户名'), 'dataIndex': 'user_name', 'width': '10%'},
+                {'title': _('全名'), 'dataIndex': 'user_full_name', 'width': '10%'},
+                {'title': _('角色'), 'dataIndex': 'user_roles', 'renderOptions': {'renderType': 'select'}, 'width': '55%'},
+            ],
+            data=[
+                {
+                    'key': f"{i['group_name']}+{i['user_name']}",
+                    'group_name': i['group_name'],
+                    'group_remark': i['group_remark'],
+                    'user_name': i['user_name'],
+                    'user_full_name': i['user_full_name'],
+                    'user_roles': {'options': [{'label': group_role, 'value': group_role} for group_role in i['group_roles']], 'mode': 'multiple', 'value': i['user_roles']},
+                }
+                for i in dao_user.get_dict_group_name_users_roles(menu_access.user_name)
+            ],
+            pageSize=10,
+        ),
+        style={'width': '100%'},
+    )
