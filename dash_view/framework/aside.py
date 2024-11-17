74bc11b18b2d0ffaf14fdf52e255ce359106d1c3
luojiaaoo <675925864@qq.com>
2024-11-17 22:55:31 +0800

    优化标题折叠动画

--- dash_view/framework/aside.py
+++ dash_view/framework/aside.py
@@ -1,45 +1,35 @@
 import feffery_antd_components as fac
+import feffery_utils_components as fuc
 from dash import get_asset_url
 from config.dash_melon_conf import ShowConf
 from common.utilities.util_menu_access import MenuAccess
+
 
 def render_aside_content(menu_access: MenuAccess):
     return fac.AntdCol(
         [
             # logo 和 app名
             fac.AntdRow(
-                [
-                    fac.AntdCol(
+                fac.AntdSpace(
+                    [
                         fac.AntdImage(
                             width=40,
                             height=40,
                             src=get_asset_url('imgs/logo.png'),
                             preview=False,
                         ),
-                        flex='1',
-                        className={
-                            'height': '100%',
-                            'alignItems': 'center',
-                        },
-                    ),
-                    fac.AntdCol(
                         fac.AntdText(
                             ShowConf.APP_NAME,
+                            id='logo-text',
+                            ellipsis=True,
                             className={
                                 'fontSize': '20px',
                                 'fontWeight': 'bold',
                                 'color': 'rgb(245,245,245)',
                             },
                         ),
-                        flex='5',
-                        className={
-                            'height': '100%',
-                            'alignItems': 'center',
-                            'marginLeft': '20px',
-                        },
-                        id='logo-text',
-                    ),
-                ],
+                    ]
+                ),
                 className={
                     'height': '60px',
                     'background': 'rgb( 43, 47, 58)',
@@ -69,7 +59,7 @@ def render_aside_content(menu_access: MenuAccess):
         ],
         # 修改目录的样式
         className={
-            '.ant-menu-submenu-title, .ant-menu': {'backgroundColor': 'rgb( 43, 47, 58)','overflow':'hidden'},
+            '.ant-menu-submenu-title, .ant-menu': {'backgroundColor': 'rgb( 43, 47, 58)', 'overflow': 'hidden'},
             '.ant-menu-submenu-title:hover': {'color': '#fff'},
             '.ant-menu-item-selected': {
                 'backgroundColor': 'rgba(0,0,0,0)',
