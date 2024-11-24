db4e18c0b64c23d4f33209df0197327333d653c8
luojiaaoo <675925864@qq.com>
2024-11-24 21:15:55 +0800

    添加peewee自动管理连接上下文/数据库重连

--- database/sql_db/conn.py
+++ database/sql_db/conn.py
@@ -1,13 +1,40 @@
 from config.dash_melon_conf import SqlDbConf
 from playhouse.pool import PooledMySQLDatabase
+from server import server
+from playhouse.shortcuts import ReconnectMixin
 
 
-db = PooledMySQLDatabase(
-    database=SqlDbConf.DATABASE,
-    max_connections=SqlDbConf.POOL_SIZE,
-    user=SqlDbConf.USER,
-    password=SqlDbConf.PASSWORD,
-    host=SqlDbConf.HOST,
-    port=SqlDbConf.PORT,
-    stale_timeout=300,
-)
+# 断线重连+连接池
+class ReconnectPooledMySQLDatabase(ReconnectMixin, PooledMySQLDatabase):
+    _instance = None
+
+    @classmethod
+    def get_db_instance(cls):
+        if not cls._instance:
+            cls._instance = cls(
+                database=SqlDbConf.DATABASE,
+                max_connections=SqlDbConf.POOL_SIZE,
+                user=SqlDbConf.USER,
+                password=SqlDbConf.PASSWORD,
+                host=SqlDbConf.HOST,
+                port=SqlDbConf.PORT,
+                stale_timeout=300,
+            )
+        return cls._instance
+
+
+def db():
+    return ReconnectPooledMySQLDatabase.get_db_instance()
+
+
+# 自动管理数据库上下文
+@server.before_request
+def _db_connect():
+    db().connect()
+
+
+@server.teardown_request
+def _db_close(exc):
+    _db = db()
+    if not _db.is_closed():
+        _db.close()
