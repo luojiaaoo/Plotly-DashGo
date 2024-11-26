4648b27d2687b8fda471e5bb1cad66bbd67bbdfd
luoja <luoja@dfmc.com.cn>
2024-11-26 10:32:22 +0800

    delete no need

--- scripts/babel/init_po.bat
+++
@@ -1,3 +0,0 @@
-cd /d %~dp0/../..
-pybabel extract -o translations/messages.pot .
-pybabel init -i translations/messages.pot -d translations -l en
--- scripts/babel/update_po.bat
+++
@@ -1,4 +0,0 @@
-cd /d %~dp0/../..
-pybabel extract -o translations/messages.pot .
-pybabel update -i translations/messages.pot -d translations
-pybabel compile -d translations
