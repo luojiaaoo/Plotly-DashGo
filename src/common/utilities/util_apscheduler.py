2bff1a7060be2b42114b1ed2bc931baf08c77095
luojiaaoo <675925864@qq.com>
2025-04-01 23:24:40 +0800

    update

--- src/common/utilities/util_apscheduler.py
+++ src/common/utilities/util_apscheduler.py
@@ -17,3 +17,36 @@ def add_ssh_interval_job(ip, password, script_text, interval, timeout, job_id, m
         raise e
     finally:
         conn.close()
+
+
+def add_ssh_cron_job(ip, password, script_text, cron_text, timeout, job_id, max_instances=10, year=None, week=None):
+    """https://apscheduler.readthedocs.io/en/master/api.html#apscheduler.triggers.cron.CronTrigger"""
+    try:
+        conn = rpyc.connect('localhost', 8091)
+        if len(cron_text) == 5:
+            minute, hour, day, month, day_of_week = cron_text
+            second = None
+        elif len(cron_text) == 6:
+            second, minute, hour, day, month, day_of_week = cron_text
+        else:
+            raise Exception('cron_text error')
+        job = conn.root.add_job(
+            'server:ssh_run_script',
+            'cron',
+            args=[ip, password, script_text, timeout],
+            year=year,
+            week=week,
+            second=second,
+            minute=minute,
+            hour=hour,
+            day=day,
+            month=month,
+            day_of_week=day_of_week,
+            job_id=job_id,
+            max_instances=max_instances,
+        )
+        return job.id
+    except Exception as e:
+        raise e
+    finally:
+        conn.close()
--- src_apscheduler/server.py
+++ src_apscheduler/server.py
@@ -8,25 +8,25 @@ class SchedulerService(rpyc.Service):
     def exposed_add_job(self, func, *args, **kwargs):
         return scheduler.add_job(func, *args, **kwargs)
 
-    def exposed_modify_job(self, job_id, jobstore=None, **changes):
+    def exposed_modify_job(self, job_id, jobstore='sqlalchemy', **changes):
         return scheduler.modify_job(job_id, jobstore, **changes)
 
-    def exposed_reschedule_job(self, job_id, jobstore=None, trigger=None, **trigger_args):
+    def exposed_reschedule_job(self, job_id, jobstore='sqlalchemy', trigger=None, **trigger_args):
         return scheduler.reschedule_job(job_id, jobstore, trigger, **trigger_args)
 
-    def exposed_pause_job(self, job_id, jobstore=None):
+    def exposed_pause_job(self, job_id, jobstore='sqlalchemy'):
         return scheduler.pause_job(job_id, jobstore)
 
-    def exposed_resume_job(self, job_id, jobstore=None):
+    def exposed_resume_job(self, job_id, jobstore='sqlalchemy'):
         return scheduler.resume_job(job_id, jobstore)
 
-    def exposed_remove_job(self, job_id, jobstore=None):
+    def exposed_remove_job(self, job_id, jobstore='sqlalchemy'):
         scheduler.remove_job(job_id, jobstore)
 
     def exposed_get_job(self, job_id):
         return scheduler.get_job(job_id)
 
-    def exposed_get_jobs(self, jobstore=None):
+    def exposed_get_jobs(self, jobstore='sqlalchemy'):
         return scheduler.get_jobs(jobstore)
 
 
