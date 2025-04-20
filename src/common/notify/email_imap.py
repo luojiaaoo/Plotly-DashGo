3550db50ef07b72f53337d6dda738495c6d05599
NOKIAO <675925864@qq.com>
2025-04-20 22:24:59 +0800

    feature: 添加IMAP邮件接收渠道，用于主动触发器

---
+++ src/common/notify/email_imap.py
@@ -0,0 +1,85 @@
+import imaplib
+import email
+from email.header import decode_header
+from datetime import datetime
+from common.utilities.util_logger import Log
+
+logger = Log.get_logger(__name__)
+
+
+def get_email_context_from_subject(
+    imap_server: str,
+    port: int,
+    emal_account: str,
+    password: str,
+    subject: str,
+    since_time: datetime,
+    before_time: datetime,
+):
+    context = ''
+    mail = imaplib.IMAP4_SSL(imap_server, port=port)
+    try:
+        mail.login(emal_account, password)
+        mail.select('inbox')  # 选择邮箱文件夹（通常是 INBOX）
+        since_time = f'{since_time:%d-%b-%Y}'
+        before_time = f'{before_time:%d-%b-%Y}'
+        status, messages = mail.search(None, f'SUBJECT "{subject}" SINCE {since_time} BEFORE {before_time}')
+        messages = messages[0].split()
+        emails = []
+        for mail_id in messages:
+            status, msg_data = mail.fetch(mail_id, '(RFC822)')
+            for response_part in msg_data:
+                if isinstance(response_part, tuple):
+                    msg = email.message_from_bytes(response_part[1])  # 解析邮件内容
+                    subject, encoding = decode_header(msg['Subject'])[0]  # 获取邮件主题
+                    if isinstance(subject, bytes):
+                        subject = subject.decode(encoding or 'utf-8')
+                    from_info = ''
+                    for from_, encoding in decode_header(msg.get('From')):  # 获取发件人
+                        if isinstance(from_, bytes):
+                            from_info += from_.decode(encoding or 'utf-8')
+                    date_str = msg.get('Date', '')[:25]  # 提取邮件发送时间
+                    try:
+                        email_datetime = datetime.strptime(date_str, '%a, %d %b %Y %H:%M:%S')
+                    except ValueError as e:
+                        logger.error(f'无法解析邮件的发送时间的日期字符串: {date_str} err: {e}')
+                        continue
+                    if not (since_time <= email_datetime < before_time):  # 过滤精确时间
+                        continue
+                    if msg.is_multipart():  # 如果邮件是多部分的
+                        for part in msg.walk():
+                            content_type = part.get_content_type()
+                            content_disposition = str(part.get('Content-Disposition'))
+                            if content_type == 'text/plain' and 'attachment' not in content_disposition:
+                                body = part.get_payload(decode=True).decode(part.get_content_charset() or 'utf-8')
+                                context += body
+                    else:
+                        body = msg.get_payload(decode=True).decode(msg.get_content_charset() or 'utf-8')
+                        context += body
+            emails.append(
+                {
+                    'subject': subject,
+                    'datetime': email_datetime,
+                    'from': from_info,
+                    'context': context,
+                }
+            )
+        return emails
+    except Exception as e:
+        raise e
+    finally:
+        mail.logout()  # 关闭连接
+
+
+# a = get_email_context_from_subject(
+#     imap_server=IMAP_SERVER,
+#     port=993,
+#     emal_account=EMAIL_ACCOUNT,
+#     password=PASSWORD,
+#     subject='VDU',
+#     since_time=datetime.now() - timedelta(days=5),
+#     before_time=datetime.now(),
+# )
+# from pprint import pprint
+
+# pprint(a)
