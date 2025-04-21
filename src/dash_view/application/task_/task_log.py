fc82b70cb5773f98c9c097c53861100dd566869a
NOKIAO <675925864@qq.com>
2025-04-21 12:26:24 +0200

    添加style拉伸运行视窗

--- src/dash_view/application/task_/task_log.py
+++ src/dash_view/application/task_/task_log.py
@@ -19,68 +19,78 @@ access_metas = ('任务日志-页面',)
 
 
 def render_content(menu_access: MenuAccess, **kwargs):
-    return fuc.FefferyInViewport(
-        Card(
-            fac.AntdSpace(
-                [
-                    fac.AntdSpace(
-                        [
-                            fac.AntdSelect(
-                                id='task-log-job-id-select',
-                                options=sorted([{'label': job.job_id, 'value': job.job_id} for job in get_apscheduler_all_jobs()], key=lambda job: job['label'], reverse=True),
-                                style={'width': 800},
-                                listHeight=200,
-                                autoClearSearchValue=True,
-                                prefix=fac.AntdIcon(icon='bi-table'),
-                            ),
-                            fac.AntdButton(t__task('查询执行记录'), id='task-log-get-start-datetime-btn'),
-                        ]
-                    ),
-                    fac.AntdSpace(
-                        [
-                            fac.AntdSelect(
-                                id='task-log-start-datetime-select',
-                                prefix=fac.AntdIcon(icon='md-query-builder'),
-                                listHeight=500,
-                                autoClearSearchValue=True,
-                                style={'width': 800},
-                            ),
-                            fac.AntdButton(t__task('查询执行日志'), id='task-log-get-log-btn'),
-                            fac.AntdSwitch(
-                                checked=True,
-                                id='task-log-scroll-log',
-                                checkedChildren=t__task('自动滚动'),
-                                unCheckedChildren=t__task('关闭滚动'),
-                            ),
-                        ]
-                    ),
-                    fac.Fragment(id='task-log-sse-container'),
-                    fuc.FefferyScroll(
-                        id='task-log-scroll-bottom',
-                        scrollMode='to-bottom',
-                        containerId='task-log-command-output-records',
-                    ),
-                    html.Div(
-                        '---',
-                        id='task-log-command-output-records',
-                        style={
-                            'width': '95%',
-                            'background': 'black',
-                            'color': 'white',
-                            'padding': '6px 8px',
-                            'whiteSpace': 'pre',
-                            'height': '95%',
-                            'overflow': 'auto',
-                        },
-                    ),
-                ],
-                size='middle',
-                style={'width': '100%', 'height': '85%'},
-                className={'& :nth-child(5)': {'height': '100%'}},
-                direction='vertical',
-            ),
-            style={'width': '100%', 'height': '100%'},
-            className={'& .ant-card-body': {'height': '100%'}},
+    return [
+        fuc.FefferyStyle(
+            rawStyle="""
+            #task-log-viewport {
+                height: 100%;
+                width: 100%;
+            }
+            """
         ),
-        id='task-log-viewport',
-    )
+        fuc.FefferyInViewport(
+            Card(
+                fac.AntdSpace(
+                    [
+                        fac.AntdSpace(
+                            [
+                                fac.AntdSelect(
+                                    id='task-log-job-id-select',
+                                    options=sorted([{'label': job.job_id, 'value': job.job_id} for job in get_apscheduler_all_jobs()], key=lambda job: job['label'], reverse=True),
+                                    style={'width': 800},
+                                    listHeight=200,
+                                    autoClearSearchValue=True,
+                                    prefix=fac.AntdIcon(icon='bi-table'),
+                                ),
+                                fac.AntdButton(t__task('查询执行记录'), id='task-log-get-start-datetime-btn'),
+                            ]
+                        ),
+                        fac.AntdSpace(
+                            [
+                                fac.AntdSelect(
+                                    id='task-log-start-datetime-select',
+                                    prefix=fac.AntdIcon(icon='md-query-builder'),
+                                    listHeight=500,
+                                    autoClearSearchValue=True,
+                                    style={'width': 800},
+                                ),
+                                fac.AntdButton(t__task('查询执行日志'), id='task-log-get-log-btn'),
+                                fac.AntdSwitch(
+                                    checked=True,
+                                    id='task-log-scroll-log',
+                                    checkedChildren=t__task('自动滚动'),
+                                    unCheckedChildren=t__task('关闭滚动'),
+                                ),
+                            ]
+                        ),
+                        fac.Fragment(id='task-log-sse-container'),
+                        fuc.FefferyScroll(
+                            id='task-log-scroll-bottom',
+                            scrollMode='to-bottom',
+                            containerId='task-log-command-output-records',
+                        ),
+                        html.Div(
+                            '---',
+                            id='task-log-command-output-records',
+                            style={
+                                'width': '95%',
+                                'background': 'black',
+                                'color': 'white',
+                                'padding': '6px 8px',
+                                'whiteSpace': 'pre',
+                                'height': '95%',
+                                'overflow': 'auto',
+                            },
+                        ),
+                    ],
+                    size='middle',
+                    style={'width': '100%', 'height': '90%'},
+                    className={'& :nth-child(5)': {'height': '100%'}},
+                    direction='vertical',
+                ),
+                style={'width': '100%', 'height': '100%'},
+                className={'& .ant-card-body': {'height': '100%'}},
+            ),
+            id='task-log-viewport',
+        ),
+    ]
